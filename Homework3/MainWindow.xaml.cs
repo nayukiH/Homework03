6e87fa0079a228fd2aa0bdaf5deaf60041850dca
nayukiH <nayukicom@sina.cn>
2019-04-03 00:53:44 +0800

    first commit

---
+++ Homework3/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Homework3
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Tip tip;
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            tip = new Tip();
+        }
+
+        private void Amount_LostFocus(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void Amount_GotFocus(object sender, RoutedEventArgs e)
+        {
+            Amount.Text = "$";
+        }
+
+        private void Amount_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            performCalculation();
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            performCalculation();
+        }
+
+        private void performCalculation()
+        {
+            var selectedRadio = myStackPanel.Children.OfType<RadioButton>().FirstOrDefault(r => r.IsChecked == true);
+            if (selectedRadio != null)
+            {
+                tip.CalculateTip(Amount.Text, double.Parse(selectedRadio.Tag.ToString()));
+            }
+            if (tip != null)
+            {
+                amountToTipTextBlock.Text = tip.TipAmount;
+                Total.Text = tip.TotalAmount;
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,24 @@
+# homework3
+
+## 作业要求：
+
+1.Please build a Desktop WPF APP. for its functions please refer to the viode "ABSWP81Part9_ Tip Calculator_mid.MP4".
+
+2.Deadline of this APP is on April 4.2019. same as homework 1,2 please upload your APP source code on Github with Readme.md
+
+https://channel9.msdn.com/Series/Windows-Phone-8-1-Development-for-Absolute-Beginners
+https://channel9.msdn.com/Series/Windows-10-development-for-absolute-beginners/UWP-001-Series-Introduction?term=Bob%20Tabor&lang-en=true
+
+
+
+## 内容说明：
+
+实现一个可计算小费的windows WPF应用，用户输入总价，并基于服务优劣选择给出小费的多少，将其需支付的小费和最终的账单总价一起输出。
+
+
+
+## 成果：
+
+![](C:\Users\nayuk\Pictures\工作\c#\tipcal\说明1.JPG)
+
+![说明2](C:\Users\nayuk\Pictures\工作\c#\tipcal\说明2.JPG)
